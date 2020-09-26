a5d9b5b0a8515e5156f3d8c4b9902c38fc8e2b96
“JairPerez-A180106” <a180106@unach.mx>
2020-09-26 00:49:18 -0500

    Subiendo practica

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$('.flexslider').flexslider(
+		{
+		prevText: "",
+		nextText: "",
+		pauseOnAction: false,
+		slideshowSpeed: 4000,
+		touch: true
+	}
+	);
+
+});
